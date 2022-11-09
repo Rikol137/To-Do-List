34151e414cfc5abbe033a8397f03ceeb83e95cc4
Rikol137 <rikolkelvin@gmail.com>
2022-11-09 11:30:19 +0300

    To do list folders with fixed linters

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+// eslint-disable-next-line import/no-unresolved
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+};
